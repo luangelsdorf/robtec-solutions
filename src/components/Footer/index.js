95ddf09421f0510881635e097e68bd0177cc43c8
lulaluano <luanferreira.ifsul@gmail.com>
2021-02-02 16:17:39 -0300

    footer hotfix

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,12 +3,14 @@ import linkedin from 'assets/img/linkedin.svg';
 
 export default function () {
     return (
-        <footer className="footer d-flex w-100 justify-content-between py-4">
-            <div className="d-flex align-items-center">
-                <span className="footer-text">Copyright © 2020 – Robtec Solutions</span>
-            </div>
-            <div>
-                <img src={linkedin} alt="linkedin-logo" />
+        <footer className="footer w-100 py-4">
+            <div className="container d-flex justify-content-between">
+                <div className="d-flex align-items-center">
+                    <span className="footer-text">Copyright © 2020 – Robtec Solutions</span>
+                </div>
+                <div>
+                    <img src={linkedin} alt="linkedin-logo" />
+                </div>
             </div>
         </footer>
     )
